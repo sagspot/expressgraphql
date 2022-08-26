70c636cdf7784bae85e17dee2eac42ca3923af16
Oliver Sagala <hello@sagspot.co.ke>
2022-08-26 21:45:37 +0300

    add dataloader

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -1,10 +1,20 @@
+const DataLoader = require('dataloader');
 const { dateToString } = require('../../helpers/date');
 const Event = require('../../models/event');
 const User = require('../../models/user');
 
+const eventLoader = new DataLoader((eventIds) => events(eventIds));
+const userLoader = new DataLoader((userIds) =>
+  User.find({ _id: { $in: userIds } })
+);
+
 const events = async (eventIds) => {
   try {
     const events = await Event.find({ id: { $in: eventIds } });
+    events.sort(
+      (a, b) =>
+        eventIds.indexOf(a._id.toString()) - eventIds.indexOf(b._id.toString())
+    );
 
     return events.map((event) => transformEvent(event));
   } catch (error) {
@@ -14,8 +24,10 @@ const events = async (eventIds) => {
 
 const singleEvent = async (eventId) => {
   try {
-    const event = await Event.findById(eventId);
-    return transformEvent(event);
+    // const event = await Event.findById(eventId);
+    const event = await eventLoader.load(eventId.toString());
+    // return transformEvent(event);
+    return event;
   } catch (error) {
     throw error;
   }
@@ -23,11 +35,13 @@ const singleEvent = async (eventId) => {
 
 const user = async (userId) => {
   try {
-    const user = await User.findById(userId);
+    // const user = await User.findById(userId);
+    const user = await userLoader.load(userId.toString());
     return {
       ...user._doc,
       password: null,
-      createdEvents: events.bind(this, user.createdEvents),
+      // createdEvents: events.bind(this, user.createdEvents),
+      createdEvents: () => eventLoader.loadMany(user.createdEvents),
     };
   } catch (error) {
     throw error;
